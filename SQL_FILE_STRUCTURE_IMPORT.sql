c06863de0b543e346f0c5d2d4f522094c15f10f2
Søren Isager Høvring <51338751+sorenisager@users.noreply.github.com>
2020-05-20 07:13:17 +0200

    ADDED SQL FILE TO IMPORT
    SQL file there was missing in the initial release.

---
+++ SQL_FILE_STRUCTURE_IMPORT.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `vrli_alerts` (
+  `id` int(13) NOT NULL,
+  `hostname` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `vmw_nsxt_firewall_src` varchar(255) DEFAULT NULL,
+  `reverse_src` varchar(255) NOT NULL,
+  `vmw_nsxt_firewall_dst` varchar(255) DEFAULT NULL,
+  `reverse_dst` varchar(255) NOT NULL,
+  `vmw_nsx_firewall_protocol` varchar(255) DEFAULT NULL,
+  `vmw_nsxt_firewall_action` varchar(255) DEFAULT NULL,
+  `vmw_nsxt_firewall_dst_port` varchar(255) DEFAULT NULL,
+  `vmw_nsxt_firewall_dst_ip_port` varchar(255) DEFAULT NULL,
+  `logging_tag` varchar(255) NOT NULL,
+  `logging_tcp_flags` varchar(255) NOT NULL,
+  `alert_webhook_id` int(13) DEFAULT NULL,
+  `alert_parent_id` int(13) DEFAULT NULL,
+  `alert_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `alert_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `alert_status` enum('new','fixed','wait','duplicate') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `vrli_webhook_inbound` (
+  `id` int(13) NOT NULL,
+  `data` text NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `state` enum('new','fixed','skipped') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
